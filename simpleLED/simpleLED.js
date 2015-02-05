502b9fb65923a7342faac7c05d2bba9000cc5667
LimorL <limorl@microsoft.com>
2015-02-04 18:24:04 -0800

    Update simpleLED.js

--- simpleLED/simpleLED.js
+++ simpleLED/simpleLED.js
@@ -1,1 +1,29 @@
-vare request = require('request');
+var request = require('request'),
+    requestInterval = 500,
+    requestUrl = "http://beaconmeetup.azurewebsites.net/beacon.aspx",
+    Gpio = require('onoff'),
+    led = new Gpio(17, 'out');
+    
+var beaconId1 = "n7b6-62cs1" ;
+    beacondId2 = "5vt4-m2eyb";
+    beaconId3 = "3x39-tyt2c";
+    
+var myBeaconId = beaconId1;
+
+var requestUrl = serviceUrl + "?id=" + myBeaconId;
+    
+  function turnOn(){
+    if(led.readSync() == 0) led.writeSync(1);
+  }
+  
+  function turnOff(){
+    if(led.readSync() == 1) led.writeSync(0);
+  }
+  
+  var query = setInterval(function(){
+    request(serviceUrl, function(error, response, body)){
+      console.log(body);
+      if(body == 0) turnOff();
+      else if (vody == 1) turnOn();
+    }
+  })
